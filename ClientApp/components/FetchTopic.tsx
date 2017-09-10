7e83eab969eea6889d2e786c76f9afd3d05610c5
Zihong Chen <czihong@gmail.com>
2017-09-11 00:07:37 +0800

    add multi columns

--- ClientApp/components/FetchTopic.tsx
+++ ClientApp/components/FetchTopic.tsx
@@ -28,61 +28,38 @@ class FetchTopic extends React.Component<TopicProps, {}>{
     }
 
     private renderTopicList() {
-        return <div className="col-md-9">
-            <div className="topic-list">
-                <div className="media">
-                    <div className="media-left">
-                        <a href="#"><img className="media-object profile-image-sm" src="https://l.ruby-china.org/user/avatar/1107.jpg" /></a>
-                    </div>
-                    <div className="media-body">
-                        <div className="meida-heading" href="#">
-                            <a href="#">
+        var row = [];
+        for (var i = 0; i < 10; i++) {
+            row.push(
+                <div className="topic-list">
+                    <div className="media">
+                        <div className="media-left">
+                            <a href="#"><img className="media-object profile-image-sm" src="https://l.ruby-china.org/user/avatar/1107.jpg" /></a>
+                        </div>
+                        <div className="media-body">
+                            <div className="meida-heading" href="#">
+                                <a href="#">
+                                    <h4>
+                                        <span>文章分享 -- </span>
+                                        <span>如何使用DotNet Core快速开发SPA应用程序</span>
+                                    </h4>
+                                </a>
+                            </div>
+                            <div className="meida-heading" href="#">
                                 <h4>
-                                    <span>文章分享</span>
-                                    <span>如何使用DotNet Core快速开发SPA应用程序</span>
+                                    <a href="#">[czihong]</a>
+                                    <span>最后由</span>
+                                    <a href="#">[hedy]</a>
+                                    <span>回复于</span>
+                                    <span>[43分钟] 前</span>
                                 </h4>
-                            </a>
+                            </div>
                         </div>
-                        <div className="meida-heading" href="#">
-                            <h4>
-                                <a href="#">[czihong]</a>
-                                <span>最后由</span>
-                                <a href="#">[hedy]</a>
-                                <span>回复于</span>
-                                <span>[43分钟] 前</span>
-                            </h4>
-                        </div>
+                        <div className="media-right"><h2>32</h2></div>
                     </div>
-                    <div className="media-right"><h2>32</h2></div>
-                </div>
-
-                <div className="media">
-                    <div className="media-left">
-                        <a href="#"><img className="media-object profile-image-sm" src="https://l.ruby-china.org/user/avatar/1107.jpg" /></a>
-                    </div>
-                    <div className="media-body">
-                        <div className="meida-heading" href="#">
-                            <a href="#">
-                                <h4>
-                                    <span>文章分享</span>
-                                    <span>如何使用DotNet Core快速开发SPA应用程序</span>
-                                </h4>
-                            </a>
-                        </div>
-                        <div className="meida-heading" href="#">
-                            <h4>
-                                <a href="#">[czihong]</a>
-                                <span>最后由</span>
-                                <a href="#">[hedy]</a>
-                                <span>回复于</span>
-                                <span>[43分钟] 前</span>
-                            </h4>
-                        </div>
-                    </div>
-                    <div className="media-right"><h2>32</h2></div>
-                </div>
-            </div>
-        </div>
+                </div>);
+        }
+        return <div className="col-md-9">{row}</div>
     }
 
     private renderSidebar() {
