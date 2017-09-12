67c3671e7c6397291ae49b371a23ee70196dfbc9
Zihong Chen <czihong@gmail.com>
2017-09-12 23:55:07 +0800

    add footer

--- ClientApp/components/Layout.tsx
+++ ClientApp/components/Layout.tsx
@@ -12,6 +12,12 @@ export class Layout extends React.Component<{}, {}> {
             <div className='container'>
                 {this.props.children}
             </div>
+            <div className='footer'>
+                <div className="container">
+                    <div>本站使用<a href="https://github.com/czihong/DotNetCoreFans"> DotNetCoreFans </a>构建</div>
+                    <div>.Net Core 中文社区希望能给所有喜欢 .Net Core 和喜欢技术的朋友，提供一个交流的地方</div>
+                </div>
+            </div>
         </div>;
     }
 }
