85d04238ec45bf4ccf80ca121d285956db19802d
Zihong Chen <czihong@gmail.com>
2017-09-23 00:54:07 +0800

    header ui complete

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 // import { connect } from 'react-redux';
 // import { ApplicationState } from '../store';
 // import * as NavMenuStore from '../store/NavMenu';
-import { Nav, Navbar, NavLink, NavItem, NavbarBrand, NavbarToggler, Collapse, Button } from 'reactstrap';
+import { Nav, Navbar, NavLink, NavItem, NavbarBrand, NavbarToggler, Collapse, Button, Container } from 'reactstrap';
 
 // type NavMenuProps =
 //     NavMenuStore.NavMenuState
@@ -36,34 +36,36 @@ export class NavMenu extends React.Component<Props, State> {
 
     public render() {
         return (
-            <Navbar className="navbar-default" light toggleable>
-                <NavbarToggler right onClick={this.toggle} />
-                <NavbarBrand href="/">.Net Core 中文社区</NavbarBrand>
-                <Collapse isOpen={ this.state.isOpen } navbar>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href={'/'}> 首页 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/fetchTopic'} > 社区 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/topic'} > 话题 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/newtopic'} > 发表新话题 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/signup'} > 注册 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/signin'} > 登陆 </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href={'/user'} > 用户中心 </NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
+            <Navbar className="navbar-default navbar-dotnet" fixed="top" light toggleable>
+                <Container>
+                    <NavbarToggler right onClick={this.toggle} />
+                    <NavbarBrand href="/">.Net Core 中文社区</NavbarBrand>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href={'/'}> 首页 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/fetchTopic'} > 社区 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/topic'} > 话题 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/newtopic'} > 发表新话题 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/signup'} > 注册 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/signin'} > 登陆 </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href={'/user'} > 用户中心 </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
             </Navbar>
         );
     }
