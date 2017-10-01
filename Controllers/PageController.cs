f24f8aaf612d0d6cc9d0ec66eff25c162da0e585
Zihong Chen <czihong@gmail.com>
2017-10-01 16:42:21 +0800

    add page controller

---
+++ Controllers/PageController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using DotNetCoreFans.Models;
+using DotNetCoreFans.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreFans.Controllers
+{
+    [Route("api/[controller]")]
+    public class PageController : Controller
+    {
+        private readonly TopicService _topicService;
+
+        public PageController(TopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        public JsonResult TopicList([FromQuery] int page)
+        {
+            var topicList = _topicService.GetAllTopic(page);
+            return null;
+        }
+        
+    }
+}
